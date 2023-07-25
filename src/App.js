e8ea228a337d9e61c824083b688e6aa10dccd33e
jeffondev <jeffondev@gmail.com>
2023-07-25 22:39:49 +0900

    시작은 반이다.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import UserUses from "./components/UserUses";
+
+function App() {
+  return (
+    <div className="App">
+      <UserUses/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/UserUses.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function UserUses() {
+
+  
+  return "작성자의 소개글 보기";
+}
+
+export default UserUses;
